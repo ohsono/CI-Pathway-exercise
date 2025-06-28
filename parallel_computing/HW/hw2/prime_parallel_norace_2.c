e6192ce42b9b17066c15bcd46bfa0fddec1caba2
Hochan Son <ohsono@gmail.com>
2025-06-28 00:30:47 -0700

    hw2_submission

--- parallel_computing/HW/hw2/prime_parallel_norace_2.c
+++ parallel_computing/HW/hw2/prime_parallel_norace_2.c
@@ -1,6 +1,6 @@
 # include <stdlib.h>
 # include <stdio.h>
-#include <omp.h>
+# include <omp.h>
 
 #define MAX_THREADS 32
 
--- parallel_computing/HW/hw2/prime_serial.c
+++ parallel_computing/HW/hw2/prime_serial.c
@@ -10,8 +10,8 @@ int main ( int argc, char *argv[] ){
   for ( i = 2; i <= n; i++ ){
     for ( j = 2; j < i; j++ ){
       if ( i % j == 0 ){
-	not_primes++;
-	break;
+        not_primes++;
+        break;
       }
     }
   }
